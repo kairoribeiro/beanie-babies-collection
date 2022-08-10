af08913aa13fd47901cfda5d84ce255f0802c671
Kairo Ribeiro <kairo10@gmail.com>
2022-08-10 17:59:11 -0400

    config wsgi

--- beaniebabiescollector/wsgi.py
+++ beaniebabiescollector/wsgi.py
@@ -6,11 +6,18 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
+# from beaniebabiescollector import MyWSGIApp
 import os
+from django.conf import settings
+from whitenoise import WhiteNoise
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beaniebabiescollector.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files("/staticfiles", prefix="more-files/")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beaniebabiescollector.settings')
+
