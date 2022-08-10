461f1adb8d8206591b8a3a77a47d4e5ae3be4b62
Kairo Ribeiro <kairo10@gmail.com>
2022-08-10 18:04:05 -0400

    config wsgi 2

--- beaniebabiescollector/wsgi.py
+++ beaniebabiescollector/wsgi.py
@@ -17,7 +17,7 @@ from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files("/staticfiles", prefix="more-files/")
+# application.add_files("/staticfiles", prefix="more-files/")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beaniebabiescollector.settings')
 
